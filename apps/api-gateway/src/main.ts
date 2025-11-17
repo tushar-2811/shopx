64871b4e5a940b0f60aab27ba8379afa62d8e5ec
tushar-2811 <tusharrawat52@gmail.com>
2025-11-17 20:37:59 +0530

    add api-gateway proxy dependencies

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import swaggerUi from 'swagger-ui-express';
+import axios from 'axios';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+// cors setup
+app.use(cors({
+  origin : ["http://localhost:3000"],
+  allowedHeaders : ['Authorization' , 'Content-Type'],
+  credentials : true,
+ })
+);
+
+app.use(morgan('dev'));
+app.use(express.json({ limit : "100mb" }));
+app.use(express.urlencoded({ extended : true , limit : "100mb" }));
+app.use(cookieParser());
+app.set("trust proxy" , 1);
+
+// rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max : (req: any) => (req?.user ? 1000 : 100),
+  message : {error : "Too many requests from this IP , please try again after 15 minutes"},
+  standardHeaders : true,
+  legacyHeaders :  true,
+  keyGenerator : (req: any) => req.ip
+});
+
+app.use(limiter)
+
+app.get('/gateway-health', (req, res) => {
+  res.send({ message: 'Welcome to api-gateway!' });
+});
+
+app.use("/" , proxy("http://localhost:6001"));
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
